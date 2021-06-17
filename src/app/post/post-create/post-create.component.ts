e8f52d7bbcd90daa60d404a2faf8a522114b349f
Aic <73454440+SereneCai@users.noreply.github.com>
2021-06-17 17:43:17 +0800

    error msgs added for form

--- src/app/post/post-create/post-create.component.ts
+++ src/app/post/post-create/post-create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import {Post} from '../post.model';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-post-create',
@@ -8,13 +10,16 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 export class PostCreateComponent implements OnInit {
   enteredContent ='';
   enteredTitle ='';
-  @Output() postCreated = new EventEmitter();
-  //output turns the compoenent in something that can listen to the outside
+  @Output() postCreated = new EventEmitter<Post>();
+  //output turns the component in something that can listen to the outside
 
-  onAddPost(){
-    const post ={
-      title: this.enteredTitle,
-      content: this.enteredContent
+  onAddPost(form: NgForm){
+    if(form.invalid){
+      return;
+    }
+    const post: Post ={
+      title: form.value.title,
+      content: form.value.content
     };
 
     this.postCreated.emit(post);// pass post as argument
--- src/app/post/post-list/post-list.component.ts
+++ src/app/post/post-list/post-list.component.ts
@@ -1,18 +1,13 @@
 import { Component, OnInit, Input } from '@angular/core';
-
+import {Post} from '../post.model';
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent implements OnInit {
-  // posts =[
-  //   {title: 'First', content: "This is the first test."},
-  //   {title: 'Second', content: "This is the second test."},
-  //   {title: 'Third', content: "This is the third test."},
-  // ];
 
-  @Input() posts =[];
+  @Input() posts: Post[] =[];
 
   constructor() { }
 
