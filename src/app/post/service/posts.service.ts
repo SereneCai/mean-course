6e3273bbcc35949b1213b56e7db92039c1348195
Aic <73454440+SereneCai@users.noreply.github.com>
2021-06-19 12:02:44 +0800

    comments added

--- src/app/post/service/posts.service.ts
+++ src/app/post/service/posts.service.ts
@@ -24,12 +24,12 @@ export class PostsService {
 
   addPost(title: string, content:string){
     const post: Post ={id: null, title:title, content:content};
-    this.http.post<{message: string}>('http://localhost:3000/api/posts', post)
+    this.http.post<{message: string}>('http://localhost:3000/api/posts', post) //post as 2nd argument, which the data we want to pass
       .subscribe((responseData) =>{
         console.log(responseData.message);
+        this.posts.push(post); //storing data locally when server is successful
+        this.postsUpdated.next([...this.posts]);
       })
-    this.posts.push(post);
-    this.postsUpdated.next([...this.posts]);
   }
   //passing the info into subject(postsUpdated) --> passing/updating this.posts into subject(postsUpdated) with .next()
 
