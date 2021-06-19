e35aead81513afce8a82a8dfcdb2406d2474e530
Aic <73454440+SereneCai@users.noreply.github.com>
2021-06-19 14:03:46 +0800

    added fetch from db

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Post = require('./model/post');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.use(bodyParser.json()); //return a valid middleware to parse json data
+mongoose.connect('mongodb+srv://user_1:tester1234@blogtest.kidjf.mongodb.net/node-angular?retryWrites=true&w=majority')
+  .then(()=>{
+    console.log("connected")
+  })
+  .catch(()=>{
+    console.log("connection fail")
+  });
 
+app.use(bodyParser.json()); //return a valid middleware to parse json data
 
 app.use((req, res, next) =>{
   //to allow cross server sharing of info, the following headers are added
@@ -19,34 +27,25 @@ app.use((req, res, next) =>{
 app.post('/api/posts', (req, res, next) =>{
   const post = new Post({
     title:  req.body.title, //function of body parser
-    content: req.body.content,
+    content: req.body.content
   });
-
-  console.log(post);
+  post.save();
   res.status(201).json({
     message:"post added successfully"
   })
 });
 
 app.get('/api/posts', (req, res, next) => {
-  const posts =[
-    {
-      id: 'asd1234',
-      title: 'test1',
-      content: 'testing text one'
-    },
-    {
-      id: '4321asd',
-      title: 'test2',
-      content: 'testing text two two'
-    },
-  ];
+  Post.find()
+    .then((result)=>{
+      console.log(result);
+      //to get data of posts as json at the browser side
+      res.status(200).json({
+        message: 'hi it worked.',
+        posts: result
+      })
+    })
 
-  //to get data of posts as json at the browser side
-  res.status(200).json({
-    message: 'hi it worked.',
-    posts: posts
-  })
 });
 
 module.exports = app;
