d7bb3dc8df3fd65ed9e0fb50841bcbc94b68dece
Aic <73454440+SereneCai@users.noreply.github.com>
2021-06-21 14:54:28 +0800

    spinner added

--- src/app/post/post-create/post-create.component.ts
+++ src/app/post/post-create/post-create.component.ts
@@ -12,9 +12,11 @@ import {ActivatedRoute, ParamMap} from "@angular/router";
 export class PostCreateComponent implements OnInit {
   enteredContent ='';
   enteredTitle ='';
+  post: Post;
+  isLoading= false;
   private mode = 'create';
   private postId: string;
-  post: Post;
+
 
   //output turns the component in something that can listen to the outside
 
@@ -38,8 +40,10 @@ export class PostCreateComponent implements OnInit {
       if(paramMap.has('postId')){
         this.mode = 'edit';
         this.postId = paramMap.get('postId');
+        this.isLoading= true;
         this.postsService.getOnePost(this.postId)
           .subscribe(postData =>{
+            this.isLoading= false ;
             this.post ={id: postData._id, title: postData.title, content: postData.content}
           })   //this is to fetch the content from db and set on the page
       } else{
