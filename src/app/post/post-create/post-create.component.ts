78c1ffb349467209a4dd8b126402c3d5e20a98c0
Aic <73454440+SereneCai@users.noreply.github.com>
2021-06-24 16:23:50 +0800

    added image edit property

--- src/app/post/post-create/post-create.component.ts
+++ src/app/post/post-create/post-create.component.ts
@@ -40,7 +40,7 @@ export class PostCreateComponent implements OnInit {
           .subscribe(postData =>{
             this.isLoading= false ;
             this.post ={id: postData._id, title: postData.title, content: postData.content, imagePath: null}
-            this.form.setValue({'title': this.post.title, 'content': this.post.content});
+            this.form.setValue({'title': this.post.title, 'content': this.post.content, image: this.post.imagePath});
             //allows you to override the values for form control at the top in case you want to edit the form
           });
         //this is to fetch the content from db and set on the page
