9e68ca6b398d15706af0024804e0248474d30cc2
Aic <73454440+SereneCai@users.noreply.github.com>
2021-06-26 13:36:49 +0800

    added unique validator in userschema

--- backend/model/user.js
+++ backend/model/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
   email: {
@@ -11,5 +12,8 @@ const userSchema = mongoose.Schema({
     required: true,
   },
 });
+
+userSchema.plugin(uniqueValidator); //plugin is a feature provided by mongoose that adds an extra hook to check the schema
+//now will get an error when trying to save same emails
 
 module.exports = mongoose.model('User', userSchema);
