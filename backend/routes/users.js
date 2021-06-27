cb645c51edaae5263fd457895b9eb0466d77a95e
Aic <73454440+SereneCai@users.noreply.github.com>
2021-06-27 13:24:01 +0800

    route created for token

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,6 +2,7 @@ const express = require ('express');
 const router = express.Router();
 const User = require('../model/user');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 router.post("/signup", (req, res, next)=> {
   bcrypt.hash(req.body.password, 10 ) //the parameter, saltround
@@ -26,7 +27,34 @@ router.post("/signup", (req, res, next)=> {
 
 });
 
+router.post('/login', (req, res, next) =>{
+  User.findOne({email: req.body.email})
+    .then(user=>{
+        if(!user){
+          return res.status(401).json({
+            message: "User not found"
+          });
+        }
+       return bcrypt.compare(req.body.password, user.password) //to compare the password between input and db
+    })
+    .then(result =>{
+      if(!result){
+        return res.status(401).json({
+          message: "Authentication failed"
+        });
+      }
+      const token = jwt.sign({email: user.email, userId: user._id},
+        'secret_should_be_longer',
+        {expiresIn: "1h"});
+      //js object passing, secret key we are use, (optional)expires
+    })
+    .catch(err =>{
+      return res.status(401).json({
+        message: "User not found"
+      });
+    })
 
+})
 
 
 
