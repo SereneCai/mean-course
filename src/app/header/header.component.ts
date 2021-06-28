8cededa5bc4c1ae025f7390522fc6cf55ad3fa5f
Aic <73454440+SereneCai@users.noreply.github.com>
2021-06-28 13:50:00 +0800

    added logout function

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -14,6 +14,10 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   constructor(private authService: AuthService) { }
 
+  onLogout(){
+    this.authService.logout();
+  }
+
   ngOnInit(): void {
     this.authListenerSubs = this.authService.getAuthStatusListener()
       .subscribe(isAuthenticated =>{
