f355a1a257370bd1fc70518d932885c88b653a02
Aic <73454440+SereneCai@users.noreply.github.com>
2021-06-28 14:07:52 +0800

    added route guards

---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,24 @@
+//auth guard is an angular service
+
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from "@angular/router";
+import {Observable} from "rxjs";
+import {Injectable} from "@angular/core";
+import {AuthService} from "./auth.service";
+
+@Injectable()
+
+export class AuthGuard implements CanActivate{
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
+    const isAuth = this.authService.getIsAuthenticated();
+    if(!isAuth){
+      this.router.navigate(['/login'])
+    }
+    return isAuth;
+  }
+
+  //return true means the user can go there
+
+}
