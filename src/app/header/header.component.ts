e4eabe1e71733dbf162fe939b73d0461f6c9e5b3
Aic <73454440+SereneCai@users.noreply.github.com>
2021-06-28 13:01:47 +0800

    subscribe to auth listener added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AuthService} from "../auth/auth.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private authListenerSubs: Subscription;
+  userAuthenticated = false;
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.authListenerSubs = this.authService.getAuthStatusListener()
+      .subscribe(isAuthenticated =>{
+        this.userAuthenticated = isAuthenticated; //get the result from auth.service and set to true if received
+      });
   }
+
+  ngOnDestroy(){
+    this.authListenerSubs.unsubscribe();
+  }
+
 
 }
