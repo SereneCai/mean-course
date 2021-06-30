9260cb73a71f84d98220020f963b7843d3b05dc5
Aic <73454440+SereneCai@users.noreply.github.com>
2021-06-30 11:23:48 +0800

    added error handling for createUser

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -39,6 +39,8 @@ export class AuthService {
     return this.http.post("http://localhost:3000/api/users/signup", authData)
       .subscribe(()=>{
         this.router.navigate(["/"]);
+      }, error => {
+        this.authStatusListener.next(false);
       })
   }
 
