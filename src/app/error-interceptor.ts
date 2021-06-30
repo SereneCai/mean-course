175b491e05fa27894d47068e6ccada10e6b1de3d
scxiren <73454440+SereneCai@users.noreply.github.com>
2021-06-30 18:09:51 +0800

    added error dialog message

--- src/app/error-interceptor.ts
+++ src/app/error-interceptor.ts
@@ -3,18 +3,18 @@ import {catchError} from "rxjs/operators";
 import {throwError} from "rxjs";
 import {Injectable} from "@angular/core";
 import {MatDialog} from "@angular/material/dialog";
+import {ErrorComponent} from "./error/error.component";
 
 @Injectable()
 
 export class ErrorInterceptor implements HttpInterceptor{
 
-  constructor() {}
+  constructor(private dialog: MatDialog) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
-        console.log(error);
-        alert(error.error.error.message); //due to mongoose adding 1 more error within
+      this.dialog.open(ErrorComponent);
         return throwError(error);
       })
     );
