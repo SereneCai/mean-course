2a85a56e55c7aa9f71d447ee996fcbf6e7a46461
Aic <73454440+SereneCai@users.noreply.github.com>
2021-06-30 17:34:54 +0800

    error interceptor created

---
+++ src/app/error-interceptor.ts
@@ -0,0 +1,22 @@
+import {HttpErrorResponse, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {catchError} from "rxjs/operators";
+import {throwError} from "rxjs";
+
+export class ErrorInterceptor implements HttpInterceptor{
+
+  constructor() {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    return next.handle(req).pipe(
+      catchError((error: HttpErrorResponse) => {
+        console.log(error);
+        alert(error.error.message);
+        return throwError(error);
+      })
+    );
+  }
+}
+
+//2 arguments
+// HttpRequest<any> --> static, middleware for outgoing req
+//now all outgoing http req will be watched by this interceptor
