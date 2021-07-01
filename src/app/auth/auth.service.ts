a15025b5ced81e653ab06acf23c3f9d369a02006
scxiren <73454440+SereneCai@users.noreply.github.com>
2021-07-01 10:07:44 +0800

    added / to end of url extension

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,6 +4,10 @@ import {AuthData} from "./auth-data.model";
 import {Subject} from "rxjs";
 import {Router} from "@angular/router";
 
+import {environment} from "../../environments/environment";
+
+const BACKEND_URL= environment.apiUrl + '/users/';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -36,7 +40,7 @@ export class AuthService {
 
   createUser(email: string, password: string){
     const authData: AuthData = {email: email, password: password};
-    return this.http.post("http://localhost:3000/api/users/signup", authData)
+    return this.http.post(BACKEND_URL + "/signup", authData)
       .subscribe(()=>{
         this.router.navigate(["/"]);
       }, error => {
@@ -46,7 +50,7 @@ export class AuthService {
 
   login(email: string, password: string){
     const authData: AuthData = {email: email, password: password};
-    this.http.post<{token: string, expiresIn: number, userId: string}>("http://localhost:3000/api/users/login", authData)
+    this.http.post<{token: string, expiresIn: number, userId: string}>(BACKEND_URL + "/login", authData)
       .subscribe(response=>{
         const token = response.token; //send as json from backend from user routes
         this.token = token;
