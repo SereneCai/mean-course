eb16536bf4a70d3a1c1d3ae73516be902f6dc23d
scxiren <73454440+SereneCai@users.noreply.github.com>
2021-07-01 09:30:56 +0800

    added auth.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule} from "@angular/forms";
-import {ReactiveFormsModule} from "@angular/forms";
+import { HeaderComponent } from './header/header.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+
+import { BrowserModule } from '@angular/platform-browser';
+import {ReactiveFormsModule} from "@angular/forms";
+
 import {AngularMaterialModule} from "./angular-material.module";
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,32 +12,29 @@ import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { HeaderComponent } from './header/header.component';
 
-import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component';
+
 import {AuthInterceptor} from "./auth/auth-interceptor";
 import {ErrorInterceptor} from "./error-interceptor";
-import { ErrorComponent } from './error/error.component';
+import {ErrorComponent } from './error/error.component';
 import {PostsModule} from "./post/posts.module";
+import {AuthModule} from "./auth/auth.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LoginComponent,
-    SignupComponent,
     ErrorComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule, //unlocked template driven approach
     ReactiveFormsModule, //unlocks the reactive driven form approach
     BrowserAnimationsModule,
     HttpClientModule,
     AngularMaterialModule,
     PostsModule,
+    AuthModule,
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
