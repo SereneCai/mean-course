15e6624d445eda43e3944381fb65923bb5cc9b7e
scxiren <73454440+SereneCai@users.noreply.github.com>
2021-07-01 08:48:01 +0800

    modified multer route

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,17 +1,18 @@
 const express = require ('express');
 const router = express.Router();
 const checkAuth = require('../middleware/check-auth');
+const extractFile = require ('../middleware/multer-file');
 const PostController = require('../controllers/posts-controller');
 
 //single refers to a single file
 //execution happens from right to left, thus can pass in more than 1 paramemter to be checked
-router.post("", checkAuth, PostController.createPost);
+router.post("", checkAuth, extractFile, PostController.createPost);
 
 router.get('', PostController.fetchAllPosts);
 
 router.get('/:id',PostController.fetchOnePost )
 
-router.put('/:id', checkAuth, multer({storage: storage}).single("image"), PostController.updatePost);
+router.put('/:id', checkAuth, extractFile, PostController.updatePost);
 
 router.delete('/:id',checkAuth, PostController.deletePost);
 
