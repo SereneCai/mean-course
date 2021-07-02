4876c3860e95a1f6d180dcf6d8b87b526789434e
scxiren <73454440+SereneCai@users.noreply.github.com>
2021-07-02 11:07:31 +0800

    rearranged imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,18 @@
 import { NgModule } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-
-import { BrowserModule } from '@angular/platform-browser';
-import {ReactiveFormsModule} from "@angular/forms";
-
-import {AngularMaterialModule} from "./angular-material.module";
-
-import { AppRoutingModule } from './app-routing.module';
+import {ErrorComponent } from './error/error.component';
 import { AppComponent } from './app.component';
 
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import { BrowserModule } from '@angular/platform-browser';
+import {ReactiveFormsModule} from "@angular/forms";
+import {AngularMaterialModule} from "./angular-material.module";
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-
+import {PostsModule} from "./post/posts.module";
 
 import {AuthInterceptor} from "./auth/auth-interceptor";
 import {ErrorInterceptor} from "./error-interceptor";
-import {ErrorComponent } from './error/error.component';
-import {PostsModule} from "./post/posts.module";
 
 @NgModule({
   declarations: [
