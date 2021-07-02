f1d9fb38567fefbcbb3edbf5a52711b4435ffdd5
scxiren <73454440+SereneCai@users.noreply.github.com>
2021-07-02 11:15:15 +0800

    removed unused component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {PostListComponent} from "./post/post-list/post-list.component";
 import {PostCreateComponent} from "./post/post-create/post-create.component";
-import {LoginComponent} from "./auth/login/login.component";
-import {SignupComponent} from "./auth/signup/signup.component";
 import {AuthGuard} from "./auth/auth.guard";
 
 const routes: Routes = [
